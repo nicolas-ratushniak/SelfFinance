72d14b4b0c82583004a4942873cc6bc9af706207
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-11-10 21:30:01 +0200

    Add ExpenseTagsController

---
+++ SelfFinance.Api/Controllers/ExpenseTagsController.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using SelfFinance.Api.Dto;
+using SelfFinance.Domain.Abstract;
+using SelfFinance.Domain.Dto;
+using SelfFinance.Domain.Exceptions;
+
+namespace SelfFinance.Api.Controllers;
+
+[ApiController]
+[Route("self-finance/api/expense-tags")]
+public class ExpenseTagsController : ControllerBase
+{
+    private readonly IExpenseTagService _expenseTagService;
+    private readonly ILogger<ExpenseTagsController> _logger;
+
+    public ExpenseTagsController(IExpenseTagService expenseTagService, ILogger<ExpenseTagsController> logger)
+    {
+        _expenseTagService = expenseTagService;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllExpenseTagsAsync()
+    {
+        var tags = (await _expenseTagService.GetAllAsync())
+            .Select(tag => new ExpenseTagDto
+            {
+                Id = tag.Id,
+                Name = tag.Name
+            });
+
+        return new ObjectResult(tags);
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetExpenseTagAsync(int id)
+    {
+        try
+        {
+            var tag = await _expenseTagService.GetAsync(id);
+
+            return new ObjectResult(new ExpenseTagDto
+            {
+                Id = tag.Id,
+                Name = tag.Name
+            });
+        }
+        catch (EntityNotFoundException ex)
+        {
+            _logger.LogInformation("Handled exception: {Exception}", ex.Message);
+            return NotFound();
+        }
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddExpenseTagAsync([FromBody] ExpenseTagCreateDto dto)
+    {
+        try
+        {
+            await _expenseTagService.AddAsync(dto);
+            return Ok();
+        }
+        catch (ValidationException ex)
+        {
+            _logger.LogInformation("Handled exception: {Exception}", ex.Message);
+            return BadRequest($"Validation error occured: {ex.Message}");
+        }
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> UpdateExpenseTagAsync([FromBody] ExpenseTagUpdateDto dto)
+    {
+        try
+        {
+            await _expenseTagService.UpdateAsync(dto);
+            return Ok();
+        }
+        catch (ValidationException ex)
+        {
+            _logger.LogInformation("Handled exception: {Exception}", ex.Message);
+            return BadRequest($"Validation error occured: {ex.Message}");
+        }
+        catch (EntityNotFoundException ex)
+        {
+            _logger.LogInformation("Handled exception: {Exception}", ex.Message);
+            return NotFound();
+        }
+    }
+    
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteExpenseTagAsync(int id)
+    {
+        try
+        {
+            await _expenseTagService.SoftDeleteAsync(id);
+            return Ok();
+        }
+        catch (EntityNotFoundException ex)
+        {
+            _logger.LogInformation("Handled exception: {Exception}", ex.Message);
+            return NotFound();
+        }
+    }
+}
---
+++ SelfFinance.Api/Dto/ExpenseTagDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SelfFinance.Api.Dto;
+
+public class ExpenseTagDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
