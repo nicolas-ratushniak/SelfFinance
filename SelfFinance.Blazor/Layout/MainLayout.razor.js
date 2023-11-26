9d92b67319ccc7072a9936f53d75f5f062ec358a
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-11-26 12:47:05 +0200

    Make a layout with a side navigation

---
+++ SelfFinance.Blazor/Layout/MainLayout.razor.js
@@ -0,0 +1,24 @@
+﻿let is_open = true
+const min_width = $(".side-nav__toggle").css("width")
+
+export function ready () {
+    $(".side-nav__toggle").click(function () {is_open ? hide_nav_bar() : show_nav_bar()})
+    $(window).resize(hide_nav_bar)
+}
+
+function hide_nav_bar() {
+    $(".side-nav").css("width", min_width)
+    $(".presentation").css("margin-left", min_width)
+
+    is_open = false
+}
+
+function show_nav_bar() {
+    // max-lenght depends on a screen size, so should be checked every time triggered
+    let max_width = $(".side-nav").css("max-width")
+
+    $(".side-nav").css("width", max_width)
+    $(".presentation").css("margin-left", max_width)
+
+    is_open = true
+}
