1323ff3686cbc254fcc2d8de47a3dd1de2d5d12f
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-12-06 15:21:38 +0200

    Edit dto

--- SelfFinance.Domain/Dto/TransactionCreateDto.cs
+++ SelfFinance.Domain/Dto/TransactionCreateDto.cs
@@ -5,12 +5,17 @@ namespace SelfFinance.Domain.Dto;
 
 public class TransactionCreateDto
 {
-    [Required, Range(1, 1_000_000), JsonProperty("sum")]
+    [Required]
+    [JsonProperty("sum")]
+    [Range(1, 1_000_000, ErrorMessage = "Value must be between 1 and 1000000")]
     public decimal Sum { get; set; }
 
-    [Required, JsonProperty("operationDate")]
+    [Required]
+    [JsonProperty("operationDate")]
     public DateTime OperationDate { get; set; }
 
-    [Required, JsonProperty("operationTagId")]
+    [Required]
+    [JsonProperty("operationTagId")]
+    [Range(1, int.MaxValue, ErrorMessage = "None of tags was selected")]
     public int OperationTagId { get; set; }
 }
