882b96c72d28ff7d27b3c7e60aecb54ec796fb87
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-12-07 11:20:42 +0200

    Add tags page

---
+++ SelfFinance.Blazor/Pages/Tags.razor.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.ObjectModel;
+using SelfFinance.Blazor.Components;
+using SelfFinance.Blazor.Components.Abstract;
+using SelfFinance.Client.ViewModels;
+using SelfFinance.Domain.Dto;
+
+namespace SelfFinance.Blazor.Pages;
+
+public partial class Tags
+{
+    private ObservableCollection<OperationTagViewModel>? _tags;
+    private OperationTagViewModel? _tagToDelete;
+
+    private ModalDelete _deleteModal;
+    private TagCreateModal _addModal;
+    private TagUpdateModal _editModal;
+    
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            _tags = new ObservableCollection<OperationTagViewModel>(await OperationTagService.GetAllAsync());
+        }
+        catch (HttpRequestException)
+        {
+            // handle
+        }
+    }
+
+    private void ShowDeleteModal(OperationTagViewModel tag)
+    {
+        _tagToDelete = tag;
+        _deleteModal.Show();
+    }
+
+    private void ShowEditModal(OperationTagViewModel tag)
+    {
+        var updateDto = new OperationTagUpdateDto
+        {
+            Id = tag.Id,
+            Name = tag.Name
+        };
+
+        _editModal.Tag = updateDto;
+        _editModal.Show();
+    }
+
+    private async Task HandleAddAsync(OperationTagCreateDto dto)
+    {
+        try
+        {
+            var id = await OperationTagService.AddAsync(dto);
+            var addedTransaction = await OperationTagService.GetAsync(id);
+
+            // to avoid refreshing all items from DB
+            _tags!.Add(addedTransaction);   // todo: insert in correct order
+            _addModal.Hide();
+        }
+        catch (HttpRequestException)
+        {
+            _addModal.ErrorMessage = "Some server error occured";
+        }
+    }
+
+    private async Task HandleUpdateAsync(OperationTagUpdateDto dto)
+    {
+        try
+        {
+            await OperationTagService.UpdateAsync(dto);
+
+            var updatedTag = await OperationTagService.GetAsync(dto.Id);
+            var oldTag = _tags!.Single(t => t.Id == dto.Id);
+
+            // to avoid refreshing all items from DB
+            oldTag.Name = updatedTag.Name;
+
+            _editModal.Hide();
+        }
+        catch (HttpRequestException)
+        {
+            _editModal.ErrorMessage = "Some server error occured";
+        }
+    }
+
+    private async Task HandleDeleteModalResultAsync(ModalResult result)
+    {
+        var tagToDelete = _tagToDelete;
+        _tagToDelete = null;
+
+        if (result == ModalResult.Success)
+        {
+            try
+            {
+                await OperationTagService.SoftDeleteAsync(tagToDelete!.Id);
+                // to avoid refreshing all items from DB
+                _tags!.Remove(tagToDelete);
+            }
+            catch (HttpRequestException)
+            {
+                // handle
+            }
+        }
+    }
+}
